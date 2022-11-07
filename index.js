8f381288818fd598f95cf37d382b29ff7b14e018
Cosmin Sava <sva4296@gmail.com>
2022-11-07 20:05:27 +0200

    Follow and unffolow a user + create Posts model

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
+
 
 const app = express();
 
@@ -25,6 +27,8 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+
 
 app.listen(8800, () => {
   console.log("Backend is Running");
